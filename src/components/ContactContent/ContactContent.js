2a9ffddad7fc03c2412a4387cf333e062b18a23d
Taylor Foxx <taylor@socialdesignhouse.com>
2020-12-29 21:53:59 -0500

    Form redirects not working...

--- src/components/ContactContent/ContactContent.js
+++ src/components/ContactContent/ContactContent.js
@@ -4,40 +4,16 @@ import { hot } from "react-hot-loader";
 class ContactContent extends Component {
 	constructor(props) {
 		super(props);
-
-		this.state = { emailAttempt:false, emailSent: false, pageHead: "Contact me for any inquiries.", pageMsg: "Fill out the form below or feel free to contact me at" };
-		this.sendEmail = this.sendEmail.bind(this);
-	}
-
-	sendEmail(e) {
-		e.preventDefault();
-
-		emailjs.sendForm('webmail', 'contact_form', e.target, 'user_b75JdYWiwUTBLYNrRl6YF')
-		.then((result) => {
-			this.setState({
-				emailAttempt: true,
-				emailSent: true,
-				pageHead: "Message Sent.",
-				pageMsg: "Thank you for the inquiry. I will get back to you as soon as possible. If you have any other questions or more pressing concerns, feel free to contact me at"
-			});
-		}, (error) => {
-			this.setState({
-				emailAttempt: true,
-				emailSent: false,
-				pageHead: "Error Sending Message.",
-				pageMsg: "Something's gone wrong on my end! Sorry for the inconvenience. Try again or feel free to contact me directly at"
-			});
-		});
 	}
 
 	render() {
 		return(
 			<section className="ContactContent">
-				<div className={ this.emailSent }>
-					<h1>{ this.state.pageHead }</h1>
-					<h2>{ this.state.pageMsg } <a href="mailto:hello@taylorfoxx.dev">hello@taylorfoxx.dev</a>.</h2>
-					<div className={ this.state.emailAttempt ? "contact-form hidden" : "contact-form" }>
-						<form name="contact" method="post" action="/success">
+				<div>
+					<h1>Contact me for any inquiries.</h1>
+					<h2>Fill out the form below or feel free to contact me at <a href="mailto:hello@taylorfoxx.dev">hello@taylorfoxx.dev</a>.</h2>
+					<div className="contact-form">
+						<form name="contact" method="post" action="/thank-you">
 							<input type="hidden" name="form-name" value="contact" />
 							<div className="half-container">
 								<div className="half-input">
